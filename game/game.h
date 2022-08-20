c9ef56ac2756645655373740afeb5503d24f31e9
q1155115515 <2861441668@qq.com>
2022-08-21 01:00:07 +0800

    master

--- game/game.h
+++ game/game.h
@@ -6,3 +6,5 @@ void DisplayBoard(char board[ROW][COL],int row,int col);
 void playermove(char board[ROW][COL],int row,int col);
 void computermove(char board[ROW][COL],int row,int col);
 char iswin(char board[ROW][COL],int row,int col);
+git config --global user.email "2861441668@qq.com"
+  git config --global user.name "q1155115515"
